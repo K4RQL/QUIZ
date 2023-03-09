6e518d1702d0285a3c7cbfaab4f206a1ac778453
K4RQL <113165967+K4RQL@users.noreply.github.com>
2023-03-09 13:37:57 +0100

    Update Quiz.cpp

--- Quiz/Quiz.cpp
+++ Quiz/Quiz.cpp
@@ -1,14 +1,43 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
+#include <string>
 #include "Pytanie.h"
-#include "Quiz.h"
 
-using namespace std;
+int main() {
+    std::ifstream plik("pytania.txt");
+    if (!plik) {
+        std::cerr << "Nie udalo sie otworzyc pliku." << std::endl;
+        return 1;
+    }
 
-int main(){
-	string nazwaPliku="quiz.txt";
-	Pytanie pierwsze;
-	pierwsze.wczytajPytanie(nazwaPliku);
-	
-	return 0;
+    std::string linia;
+    while (std::getline(plik, linia)) {
+        Pytanie pytanie(linia, "", "", "", ' ');
+
+        // Ustawiamy odpowiedzi i poprawną odpowiedź za pomocą getline z obiektu plik.
+        std::getline(plik, linia);
+        pytanie.m_odpowiedzA = linia;
+
+        std::getline(plik, linia);
+        pytanie.m_odpowiedzB = linia;
+
+        std::getline(plik, linia);
+        pytanie.m_odpowiedzC = linia;
+
+        std::getline(plik, linia);
+        pytanie.m_poprawnaOdpowiedz = linia[0];
+
+        pytanie.zadajPytanie();
+
+        char odpowiedzUzytkownika;
+        std::cin >> odpowiedzUzytkownika;
+        pytanie.sprawdz(odpowiedzUzytkownika);
+
+        // Czyścimy bufor wejściowy, aby usunąć znak nowej linii pozostały w strumieniu po wpisaniu odpowiedzi użytkownika.
+        std::cin.ignore();
+    }
+
+    std::cout << "Koniec gry. Twoj wynik: " << pytanie.punktacja() << " / " << iloscPytan << std::endl;
+
+    return 0;
 }
