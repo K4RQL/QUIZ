6be0db9eeae90a3affc113e1a55e746f60094968
K4RQL <113165967+K4RQL@users.noreply.github.com>
2023-03-09 13:36:34 +0100

    Update Pytanie.h

--- Quiz/Pytanie.h
+++ Quiz/Pytanie.h
@@ -1,11 +1,23 @@
-#ifndef Pytanie_h
-#define Pytanie_h
+#ifndef PYTANIE_H
+#define PYTANIE_H
+
+#include <string>
 
 class Pytanie {
 public:
-	Pytanie(string nazwaPliku);
-	void wczytajPytanie();
-};
+    Pytanie(std::string trescPytania, std::string odpowiedzA, std::string odpowiedzB, std::string odpowiedzC, char poprawnaOdpowiedz);
+    void wczytajPytanie();
+    void zadajPytanie();
+    bool sprawdz(char odpowiedzUzytkownika);
+    int punktacja();
 
+private:
+    std::string m_trescPytania;
+    std::string m_odpowiedzA;
+    std::string m_odpowiedzB;
+    std::string m_odpowiedzC;
+    char m_poprawnaOdpowiedz;
+    int m_punktacja;
+};
 
 #endif
