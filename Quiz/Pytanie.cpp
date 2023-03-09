153bbf7244c7d4da78da090d05cb159735c4702f
K4RQL <113165967+K4RQL@users.noreply.github.com>
2023-03-09 13:36:01 +0100

    Update Pytanie.cpp

--- Quiz/Pytanie.cpp
+++ Quiz/Pytanie.cpp
@@ -1,23 +1,51 @@
+#include "Pytanie.h"
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "Pytanie.h"
 
-using namespace std;
+Pytanie::Pytanie(std::string trescPytania, std::string odpowiedzA, std::string odpowiedzB, std::string odpowiedzC, char poprawnaOdpowiedz) :
+    m_trescPytania(trescPytania), m_odpowiedzA(odpowiedzA), m_odpowiedzB(odpowiedzB), m_odpowiedzC(odpowiedzC), m_poprawnaOdpowiedz(poprawnaOdpowiedz), m_punktacja(0) {}
 
-Pytanie::Pytanie(string nazwaPliku){ : m_nazwaPliku(nazwaPliku){}
+void Pytanie::wczytajPytanie() {
+    std::cout << "Podaj tresc pytania: ";
+    std::getline(std::cin, m_trescPytania);
 
-class Pytanie {
-public:
-	void Pytanie::wczytajPytanie(string nazwaPliku) {
-		ifstream plik(m_nazwaPliku);
-		ifstream plik("quiz.txt");
-		if (plik.is_open()) {
-			string wiersz;
-			while (getline(plik, wiersz)) {
-				cout << wiersz << endl;
-			}
-			plik.close();
-		}
-	}
-};
+    std::cout << "Podaj odpowiedz A: ";
+    std::getline(std::cin, m_odpowiedzA);
+
+    std::cout << "Podaj odpowiedz B: ";
+    std::getline(std::cin, m_odpowiedzB);
+
+    std::cout << "Podaj odpowiedz C: ";
+    std::getline(std::cin, m_odpowiedzC);
+
+    std::cout << "Podaj poprawna odpowiedz (A, B lub C): ";
+    std::cin >> m_poprawnaOdpowiedz;
+
+    // Czyścimy bufor wejściowy, aby usunąć znak nowej linii pozostały w strumieniu po wpisaniu odpowiedzi użytkownika.
+    std::cin.ignore();
+}
+
+void Pytanie::zadajPytanie() {
+    std::cout << m_trescPytania << std::endl;
+    std::cout << "A) " << m_odpowiedzA << std::endl;
+    std::cout << "B) " << m_odpowiedzB << std::endl;
+    std::cout << "C) " << m_odpowiedzC << std::endl;
+}
+
+bool Pytanie::sprawdz(char odpowiedzUzytkownika) {
+  if (odpowiedzUzytkownika == m_poprawnaOdpowiedz) {
+    std::cout << "Poprawna odpowiedz!" << std::endl;
+    m_punktacja++;
+    return true;
+}
+else {
+    std::cout << "Niepoprawna odpowiedz." << std::endl;
+    return false;
+}
+}
+
+int Pytanie::punktacja() {
+return m_punktacja;
+}
+
